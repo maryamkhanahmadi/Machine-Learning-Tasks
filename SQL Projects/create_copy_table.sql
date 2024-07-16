d05fe36bb23c11857e59904da78fe0874bd1be0c
Maryam Khanahmadi <38852445+maryamkhanahmadi@users.noreply.github.com>
2024-07-16 22:53:39 +0200

    Add files via upload

---
+++ SQL Projects/create_copy_table.sql
@@ -0,0 +1,19 @@
+-- @author Maryam Khanahmadi
+-- This sample code creats a table and copies data based on specific condition
+USE sql_invoic;
+
+CREATE TABLE invoices_archived AS
+SELECT
+	i.invoice_id,
+    i.number,
+    c. name AS client,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.payment_date,
+    i.due_date
+FROM invoices i
+JOIN clients c
+	USING (client_id)
+WHERE payment_date IS NOT NULL
+    
